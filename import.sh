7b82e302f0fa5c4704e1effd89b216f3366ee68c
ingalls <nick@mapbox.com>
2014-11-18 00:13:26 -0500

    Verbosify Import Script

--- import.sh
+++ import.sh
@@ -1,46 +1,116 @@
 #!/bin/bash
 
+echo "Canvec Import v0.1.0"
+
 set -e -o pipefail
 TMP="$(dirname $0)/tmp"
 CACHE="$(dirname $0)/cache"
 
+#colours
+green="\e[32m"
+reset="\e[39m"
+red="\e[31m"
+yellow="\e[33m"
+
 mkdir -p $TMP
 mkdir -p $CACHE
 
+log() {
+    spaces=$(printf %${2}s)
+    if [ "$3" = "head" ]; then
+        echo "$spaces- $1"
+    else
+        echo -n "$spaces- $1"
+    fi
+}
+
+pass() {
+    echo -e " [${green}done${reset}]"
+}
+fail() {
+    echo -e " [${red}fail${reset}]"
+    exit 1
+}
+
 connect="psql -U postgres canvec"
 
+log "Is parllel installed?" 0
+type parallel > /dev/null 2>&1 || fail && pass
+
+log "Is ogr2ogr installed?" 0
+type ogr2ogr > /dev/null 2>&1 || fail && pass
+
+log "Is psql installed?" 0
+type psql > /dev/null 2>&1 || fail && pass
+
+log "Is wget installed?" 0
+type wget > /dev/null 2>&1 || fail && pass
+
+log "Is database empty?" 0
 if [ $(echo "\d" | psql -U postgres canvec | grep canvec_tmp | wc -l) != "0" ]; then
-    echo "Database not empty! Run ./setup.sh"
-    exit 1
+    fail
 fi
+pass
 
-if [ -e $CACHE/area ]; then
-    area=$(<$CACHE/area)
+log "Caching FTP Lists" 0 "head"
+
+#Cache/Read areas
+if [ -e $CACHE/areas ]; then
+    log "Reading Areas" 2
+    areas=$(<$CACHE/areas)
+    pass
 else
-    area=$( wget -qO- ftp://ftp2.cits.rncan.gc.ca/pub/canvec+/shp/ | grep "</a>" | sed '/^$/d;s/[[:blank:]]//g' | sed 's/^2014.*">//' | sed 's/\/<\/a>//' )
-    echo $area > $CACHE/test
+    log "Caching Areas " 2
+    areas=$( wget -qO- ftp://ftp2.cits.rncan.gc.ca/pub/canvec+/shp/ | grep "Directory" | sed '/^$/d;s/[[:blank:]]//g' | sed 's/^.*Directory.*">//' | sed 's/\/<\/a>//' )
+    echo $areas > $CACHE/areas
+    pass
 fi
 
-for file in $area; do
-    wget -nH --cut-dirs=100 -m ftp://ftp2.cits.rncan.gc.ca/pub/canvec+/shp/$file/* -P $TMP
-    subAreas=$( ls $TMP )
-    wget -nH --cut-dirs=100 -m ftp://ftp2.cits.rncan.gc.ca/pub/geobase/official/cgn/250k_shp_eng/$file/* -P $TMP
-    wget -nH --cut-dirs=100 -m ftp://ftp2.cits.rncan.gc.ca/pub/geobase/official/cgn/250k_shp_fra/$file/* -P $TMP
+#Cache/Read subareas
+log "Caching Sub Areas" 2 "head"
+for area in $areas; do
+    if [ ! -e $CACHE/$area ] || [ ! -e $CACHE/$area-en ] || [ ! -e $CACHE/$area-fr ]; then
+        log "Caching $area" 4
+        wget -qO- ftp://ftp2.cits.rncan.gc.ca/pub/canvec+/shp/$area/ | grep "File" | sed '/^$/d;s/[[:blank:]]//g' | sed 's/^.*File<ahref="//' | sed 's/">.*$//' > $CACHE/$area
+        wget -qO- ftp://ftp2.cits.rncan.gc.ca/pub/geobase/official/cgn/250k_shp_eng/$area/ | grep "File" | sed '/^$/d;s/[[:blank:]]//g' | sed 's/^.*File<ahref="//' | sed 's/">.*$//' > $CACHE/$area-en || true
+        wget -qO- ftp://ftp2.cits.rncan.gc.ca/pub/geobase/official/cgn/250k_shp_fra/$area/ | grep "File" | sed '/^$/d;s/[[:blank:]]//g' | sed 's/^.*File<ahref="//' | sed 's/">.*$//' > $CACHE/$area-fr || true
+        pass
+    fi
+done
+log "All Sub Areas Cached" 4 
+pass
+
+for area in $areas; do
+    log "Processing Area $area" 0 "head"
+    subAreas=$(<$CACHE/$area)
+    log "Downloading CanVec+" 2
+    parallel "wget -q {} -P $TMP" ::: <$CACHE/$area && pass
+    log "Downloading CGN-en" 2
+    parallel "wget -q {} -P $TMP" ::: <$CACHE/$area-en && pass
+    log "Downloading CGN-fr" 2
+    parallel "wget -q {} -p $TMP" ::: <$CACHE/$area-fr && pass
 
     for sub in $subAreas; do
+	log "Processing Sub Area $sub" 2 "head"
         subdir=$( echo "$sub" | sed 's/_shp.zip//' | sed 's/canvec_//')
         mkdir $TMP/$subdir
-        unzip -o $TMP/$sub -d $TMP/$subdir
-        unzip -o $TMP/cgn_${subdir,,}_shp_fra.zip -d $TMP/$subdir
-        unzip -o $TMP/cgn_${subdir,,}_shp_eng.zip -d $TMP/$subdir
+	
+	log "Unzipping CanVec+" 4
+        unzip -qq -o $TMP/$sub -d $TMP/$subdir || fail && pass
+	log "Unzipping CGN-fr" 4
+        unzip -qq -o $TMP/cgn_${subdir,,}_shp_fra.zip -d $TMP/$subdir || fail && pass
+	log "Unzipping CGN-en" 4
+        unzip -qq -o $TMP/cgn_${subdir,,}_shp_eng.zip -d $TMP/$subdir || fail && pass
         
         rm $TMP/$sub
         rm $TMP/cgn_*
 
-        ogr2ogr -t_srs EPSG:4326 -f "PostgreSQL" -nlt POINT -nln cgn_eng PG:"host='localhost' user='postgres' dbname='canvec'" $TMP/$subdir/001k_geoname.shp
-        ogr2ogr -t_srs EPSG:4326 -f "PostgreSQL" -nlt POINT -nln cgn_fra PG:"host='localhost' user='postgres' dbname='canvec'" $TMP/$subdir/001k_toponyme.shp
+	log "Importing CGN-en" 4
+        ogr2ogr -t_srs EPSG:4326 -f "PostgreSQL" -nlt POINT -nln cgn_eng PG:"host='localhost' user='postgres' dbname='canvec'" $TMP/$subdir/001k_geoname.shp || fail && pass
+	log "Importing CGN-fr" 4     
+	ogr2ogr -t_srs EPSG:4326 -f "PostgreSQL" -nlt POINT -nln cgn_fra PG:"host='localhost' user='postgres' dbname='canvec'" $TMP/$subdir/001k_toponyme.shp || fail && pass
         
-	    echo "
+       	echo "
             BEGIN;
             CREATE TABLE osm_pt (osm_tags HSTORE, id SERIAL);
             SELECT AddGeometryColumn('osm_pt', 'geom', '4326', 'POINT', 2);
@@ -69,9 +139,9 @@ for file in $area; do
                 ogr2ogr -append -t_srs EPSG:4326 -f "PostgreSQL" -nlt PROMOTE_TO_MULTI -nln ${layer}_pg PG:"host='localhost' user='postgres' dbname='canvec'" $layertmp
             done
 	
-		    ./map/bs_pt.sh
-            ./map/bs_ln.sh
-            ./map/bs_pg.sh
+		    ./map/${layer}_pt.sh
+            ./map/${layer}_ln.sh
+            ./map/${layer}_pg.sh
         done
     done
 done
