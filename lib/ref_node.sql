b3e0f8a70cd5c885d0f3c2808d0e2b25d198c3c7
ingalls <nick@mapbox.com>
2014-11-29 00:13:26 -0500

    Polygon Insertion

--- lib/ref_node.sql
+++ lib/ref_node.sql
@@ -2,19 +2,20 @@ CREATE OR REPLACE FUNCTION ref_node(poly GEOMETRY, tags HSTORE) RETURNS VOID AS 
 DECLARE
     nodeArray BIGINT[] := '{}';
     point GEOMETRY;
-    id BIGINT;
+    rings INTEGER;
 BEGIN
     IF GeometryType(poly) = 'LINESTRING' THEN 
-        FOR point IN 
-            SELECT (ST_DumpPoints(poly)).geom 
-        LOOP
-            id := ('x'||substr(md5(point::TEXT), 0, 9))::bit(32)::BIGINT; 
-            nodeArray := array_append(nodeArray, id);
+        FOR point IN SELECT (ST_DumpPoints(poly)).geom LOOP
+            nodeArray := array_append(nodeArray, idx(poly));
         END LOOP;
         INSERT INTO ways(nodes, tags) VALUES (nodeArray, tags);
     ELSIF GeometryType(poly) = 'POLYGON' THEN
-        IF (SELECT ST_NumInteriorRing(poly)) = 0 THEN
-            RAISE NOTICE 'WAY';
+        IF ST_NumInteriorRings(poly) = 0 THEN
+            RAISE NOTICE 'FUCK';
+            FOR point IN SELECT (ST_DumpPoints(poly)).geom LOOP
+                nodeArray := array_append(nodeArray, idx(poly));
+            END LOOP;
+            INSERT INTO ways(nodes, tags) VALUES (nodeArray, tags);
         ELSE
             RAISE NOTICE 'RELATION';
         END IF; 
